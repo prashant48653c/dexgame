142acf7c1ca24e855bd8c1cafcf470169c83f164
prashant acharya <acharyaprashant227@gmail.com>
2023-08-09 08:38:48 +0545

    minor interaction

--- src/component/Sidenav.jsx
+++ src/component/Sidenav.jsx
@@ -1,33 +1,38 @@
-import React from 'react'
+import React,{useState} from 'react'
 import chat from '../assets/chat.svg'
 import { useNavigate,Link } from 'react-router-dom'
 
 const Sidenav = () => {
  const navigate=useNavigate();
 
+const [activebtn, setactivebtn] = useState(0)
+const handleButtonClick = (buttonIndex) => {
+  setactivebtn(buttonIndex);
+};
+
   return (
-    <aside className='sidenav'>
+    <aside className='sidenav' >
 
 
-<div className="side-icons active">
+<div className={`side-icons  ${activebtn === 0 ? "active":""}`  }  onClick={() => handleButtonClick(0)}  >
 
-  <Link to="/"> <img className='side-icon active-icon '  src={chat} alt="community" /></Link> 
-
-</div>
-
-<div className="side-icons">
- <Link  to="/community">  <img className='side-icon  ' src={chat} alt="community" /></Link>
+ <img className={` side-icon `} onClick={()=>navigate("/")}  src={chat} alt="community"  />
 
 </div>
 
-<div className="side-icons">
-<Link to="/store">  <img className='side-icon  ' src={chat} alt="community" /></Link>
+<div className={`side-icons  ${activebtn === 1 ? "active":""}` }   onClick={() => handleButtonClick(1)}>
+    <img className='side-icon ' onClick={()=>navigate("/community")}  src={chat} alt="community" /> 
+
+</div>
+
+<div className={`side-icons  ${activebtn === 2 ? "active":""}` }   onClick={() => handleButtonClick(2)}>
+  <img className='side-icon  ' onClick={()=>navigate("/store")}  src={chat} alt="store" /> 
 
 
 </div>
 
-<div className="side-icons">
-<Link  to="/profile">  <img className='side-icon  ' src={chat} alt="community" /></Link>
+<div className={`side-icons  ${activebtn === 3 ? "active":""}` }   onClick={() => handleButtonClick(3)}>
+  <img className='side-icon' onClick={()=>navigate("/unlock")}  src={chat} alt="community" /> 
 
 
 </div>
